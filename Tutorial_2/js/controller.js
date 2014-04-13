0737c6807903b8972d7609148ccd28898cc016e9
rosy1014 <rosy1014@163.com>
2014-04-13 13:31:00 -0700

    finied tutorial 4

--- Tutorial_2/js/controller.js
+++ Tutorial_2/js/controller.js
@@ -6,9 +6,11 @@ var phonecatApp = angular.module('phonecatApp',[]);
 
 phonecatApp.controller('PhoneListCtrl', function ($scope){
     $scope.phones = [
-        {'name': 'Nexus S', 'snippet' :'Fast just got faster with Nexus S.'},
-        {'name': 'Motorola XOOM with Wi-Fi', 'snippet': 'The Next, Next Generation tablet.'},
-        {'name': 'Motorola XOOM', 'snippet': 'The Next, Next Generation tablet.'}
+        {'name': 'Nexus S', 'snippet' :'Fast just got faster with Nexus S.', 'age':1},
+        {'name': 'Motorola XOOM with Wi-Fi', 'snippet': 'The Next, Next Generation tablet.','age':2},
+        {'name': 'Motorola XOOM', 'snippet': 'The Next, Next Generation tablet.','age':3}
     ];
+    $scope.orderProp='age'
+    $scope.name = "World";
 });
-$scope.name = "World";
+
--- Tutorial_2/test/unit/controllerSpec.js
+++ Tutorial_2/test/unit/controllerSpec.js
@@ -3,14 +3,20 @@
  */
 
 describe('PhoneListCtrl', function(){
+    var scope, ctrl;
 
     beforeEach(module('phonecatApp'));
 
-    it('should create "phones" model with 3 phones', inject(function($controller){
-        var scope = {},
-            ctrl = $controller('PhoneListCtrl', {$scope:scope});
-
-        expect(scope.phones.length).toBe(4);
-        expect(scope.name).toBe('World');
+    beforeEach(inject(function($controller){
+        scope ={};
+        ctrl=$controller('PhoneListCtrl', {$scope:scope});
     }));
+
+    it('should create "phones" model with 3 phones', function(){
+        expect(scope.phons.length).toBe(3);
+    });
+
+    it('should set the default value of orderProp model', function(){
+        expect(scope.orderProp).toBe('age');
+    });
 });
