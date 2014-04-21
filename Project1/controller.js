c2bba71ffe1979bfb4a8d00da817520e42b4fee6
rosy1014 <rosy1014@163.com>
2014-04-21 00:23:06 -0700

    troubled version

--- Project1/controller.js
+++ Project1/controller.js
@@ -2,12 +2,13 @@
  * Created by RuoxiLu on 4/16/14.
  */
 
-var rosterList = angular.module('rosterList', []);
-
-rosterList.controller('RosterCtrl', function ($scope, $http) {
+//var rosterList = angular.module('rosterList', []);
+var roster = angular.module('rosterList', []);
+roster.controller('RosterCtrl', function ($scope, $http) {
     $http.get('all_user.json').success(function(data) {
         $scope.user = data;
     });
 
-    $scope.orderProp = 'username'[0,6];
+    $scope.orderProp = 'username[0:6]';
 });
+
