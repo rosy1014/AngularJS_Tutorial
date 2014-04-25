49691ef6d3d828f8321207e09b5d10665ac2f04c
rosy1014 <rosy1014@163.com>
2014-04-24 17:41:21 -0700

    selector works

--- Project1/controller.js
+++ Project1/controller.js
@@ -2,13 +2,35 @@
  * Created by RuoxiLu on 4/16/14.
  */
 
-//var rosterList = angular.module('rosterList', []);
-var roster = angular.module('rosterList', []);
+
+var roster = angular.module('roster', []);
+
 roster.controller('RosterCtrl', function ($scope, $http) {
-    $http.get('all_user.json').success(function(data) {
-        $scope.user = data;
+    $http.get('all_user.json').success(function (data) {
+        $scope.users = data;
+
+        $scope.classList = function () {
+            var my_classList = new Array();
+            var my_username;
+            for (var i = 0; i < data.length; i++) {
+                my_username = data[i].username.substring(0, 6);
+                if ($.inArray(my_username, my_classList) === -1) {
+                    my_classList.push(my_username);
+                }
+            }
+            return my_classList;
+        }
     });
 
-    $scope.orderProp = 'username[0:6]';
-});
 
+//        $scope.correctNumber = function() {
+//
+//            var my_number;
+//
+//            my_number = this.username.substring(6, 8);
+//            return _.equals(my_number);
+//
+//        }
+    $scope.orderProp = $scope.users;
+
+});
